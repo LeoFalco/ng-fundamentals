84670ef42a873c95b4b9fe96ba9d159159aceb4d
Leo Falco <leonardo.falco@outlook.com>
2020-02-26 16:45:27 -0300

    add styles

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'events-app',
-  template: '<h2>Hello world</h2>',
+  selector: 'app-events',
+  template: `
+    <h2>Hello world</h2>
+    <img src="assets/basic-shield.png"/>
+  `,
 })
 export class EventsAppComponent {
   title = 'events-ng-fundamentals';
