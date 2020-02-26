43bd35291f6ca530f99fe0a6d289453a1638a5b8
Leo Falco <leonardo.falco@outlook.com>
2020-02-26 15:36:39 -0300

    events app refactor

---
+++ src/app/events-app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    EventsAppComponent
+  ],
+  providers: [],
+  bootstrap: [EventsAppComponent]
+})
+export class EventsAppModule { }
