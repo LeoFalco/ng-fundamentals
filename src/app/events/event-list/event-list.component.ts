3722f0baaa53b57df47552b4cc748fca792353a8
Leo Falco <leonardo.falco@outlook.com>
2020-02-27 10:01:49 -0300

    refatorando pastas

---
+++ src/app/events/event-list/event-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.css']
+})
+export class EventListComponent {
+  event = {
+    id: 1,
+    name: 'Angular connect',
+    date: '9/26/2026',
+    time: '10:00 am',
+    price: 599.99,
+    iamgeUrl: 'assets/images/angularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  };
+
+
+  constructor() {
+    console.log(this.event);
+  }
+
+}
