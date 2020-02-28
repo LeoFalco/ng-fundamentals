c70fd3bd483ecca4ce53815023e3cfd284e9ccd0
Leonardo Jesus Falco <leonardo.falco@sifat.com.br>
2020-02-27 23:17:01 -0300

    fix test

--- src/app/services/toastr/toastr.service.spec.ts
+++ src/app/services/toastr/toastr.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ToastService } from './toast.service';
+import { ToastrService } from './toastr.service';
 
 describe('ToastService', () => {
-  let service: ToastService;
+  let service: ToastrService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ToastService);
+    service = TestBed.inject(ToastrService);
   });
 
   it('should be created', () => {
