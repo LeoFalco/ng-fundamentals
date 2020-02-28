068067492bed3486bef908b164f5b9736f388614
Leonardo Jesus Falco <leonardo.falco@sifat.com.br>
2020-02-27 21:17:33 -0300

    .

--- src/app/components/event-thumbnail/event-thumbnail.component.ts
+++ src/app/components/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Event } from './../../model/model';
+import { Event } from './../../models/model';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
--- src/app/events-app-root.module.ts
+++ src/app/events-app-root.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { RootComponent } from './root/root.component';
-import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RootComponent } from './components/root/root.component';
+import { EventThumbnailComponent } from './components/event-thumbnail/event-thumbnail.component';
+import { EventListComponent } from './components/event-list/event-list.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   imports: [
