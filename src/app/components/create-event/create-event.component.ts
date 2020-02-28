7cfee2cbb823e1eef145139f4cc31db1c07b9132
Leonardo Jesus Falco <leonardo.falco@outlook.com>
2020-02-28 13:20:55 -0300

    navigation from code

--- src/app/components/create-event/create-event.component.ts
+++ src/app/components/create-event/create-event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-event',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEventComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+  cancel() {
+    console.log('cancel')
+    this.router.navigate(['/events'])
+  }
 }
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,3 +1,4 @@
+import { CreateEventComponent } from './../components/create-event/create-event.component';
 import { Routes } from "@angular/router";
 
 import { EventDetailComponent } from './../components/event-detail/event-detail.component';
@@ -5,16 +6,24 @@ import { EventListComponent } from './../components/event-list/event-list.compon
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/events',
+    pathMatch: 'full'
+  },
+  {
     path: 'events',
     component: EventListComponent
+  },
+  {
+    // needs to come first from route /events/:id
+    path: 'events/new',
+    component: CreateEventComponent
   },
   {
     path: 'events/:id',
     component: EventDetailComponent
   },
-  {
-    path: '',
-    redirectTo: '/events',
-    pathMatch: 'full'
-  }
+
+
+
 ]
