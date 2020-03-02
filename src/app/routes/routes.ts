d2944fdf393a3f271413cb2d720584d794ba273f
Leonardo Jesus Falco <leonardo.falco@outlook.com>
2020-03-01 23:47:36 -0300

    fix imports

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,7 +1,7 @@
 import { CreateSessionComponent } from './../modules/main/components/create-session/create-session.component';
 import { EventResolverService } from './../services/event-resolver/event-resolver.service';
 import { EventListResolverService } from './../services/event-list-resolver/event-list-resolver.service';
-import { EventRouteActivatorService } from './../services/event-route-activator/event-route-activator.service';
+import { EventRouteActivatorService } from './../services/guard/event-route-activator/event-route-activator.service';
 import { Routes } from '@angular/router';
 import { EventListComponent } from '../modules/main/components/event-list/event-list.component';
 import { CreateEventComponent } from '../modules/main/components/create-event/create-event.component';
--- src/app/routes/user-routes.ts
+++ src/app/routes/user-routes.ts
@@ -1,6 +1,6 @@
 import { CreateAccountComponent } from './../modules/user/components/create-account/create-account.component';
-import { UserLoginActivatorService } from './../services/user-login-activator/user-login-activator.service';
-import { UserProfileActivatorService } from './../services/user-profile-activator/user-profile-activator.service';
+import { UserLoginActivatorService } from './../services/guard/user-login-activator/user-login-activator.service';
+import { UserProfileActivatorService } from './../services/guard/user-profile-activator/user-profile-activator.service';
 import { LoginComponent } from './../modules/user/components/login/login.component';
 import { UserProfileComponent } from './../modules/user/components/user-profile/user-profile.component';
 import { Routes } from '@angular/router';
